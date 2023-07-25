0959ded6ba30b0792969d678b2f18066da6b41cb
Armando Ramirez Moya <94202548+Armando-Moya@users.noreply.github.com>
2023-07-25 13:27:12 -0600

    Add files via upload

--- contador.v
+++ contador.v
@@ -1,36 +1,22 @@
-module contador #(parameter BITS=4  )
+module contador #(parameter BITS=4)
 (
  input clk,
  input rst,
  input en,
  input clr,
- output reg [BITS-1:0] Out
-
-
-);
-
-wire clk_buff;
-DIV  #(.COUNTER_SIZE(26))
-div(
-.clk(clk),
-.rst(rst),
-.en(en),
-.clkdiv(clk_buff)
-   
+ output reg [(BITS-1):0] Out
 );
 
 
-always@(posedge clk_buff or negedge rst)
+always@(posedge clk or negedge rst)
 begin
 	if(!rst)
-	  Out <= {BITS{1'B0}};//4'b0000;
-	else
-		if(en)
+	  Out <= {BITS{1'b0}};
+	else if(en)
 			if(!clr)
 					Out <= Out + 1'b1;
-				else
+			else
 					Out <= {BITS{1'b0}};
-
 end 
 
 
